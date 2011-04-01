a6ab062a718ba444f01cfb4a2cb14dd64a59548b
arfrank <arfrank@gmail.com>
2011-04-01 11:42:40 -0400

    Removing circular ref

--- app/model/locations.py
+++ app/model/locations.py
@@ -1,14 +1,14 @@
 from tipfy.appengine import db as tipfydb
 from google.appengine.ext import db
 from model.base import BaseModel
-from model.hashes import Hash
 
 class Location(BaseModel):
 	family = db.StringProperty(default = None)
 	device = db.StringProperty(default = None)
 	version = db.StringProperty(default = None)
 
-	hash = db.ReferenceProperty(Hash)
+	#will always reference the hash property, just cant put it hear due to circular reference
+	hash = db.ReferenceProperty()
 
 	location = db.TextProperty()
 	hits = db.IntegerProperty(default = 0)
